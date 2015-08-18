4501ed713b248ac7fb27b4fd9d77bf0b0fb18fca
Xavier <lefebvrex627@gmail.com>
2015-08-18 08:06:20 +0200

    Add compression and production mode

--- server.js
+++ server.js
@@ -5,20 +5,23 @@ var mongoose = require('mongoose');                     // mongoose for mongodb
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var morgan = require('morgan');
+var compression = require('compression');
 // configuration =================
 
 var nconf = require('nconf');
 nconf.file({file: 'config.json'});
 
 mongoose.connect(nconf.get('mongodbPath'));
-
+app.use(compression())
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use("/media", express.static(nconf.get('stuffPath')));
 app.use(bodyParser.urlencoded({'extended': 'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride());
-app.use(morgan('dev'));
+if (app.get('env') !== 'production') {
+    app.use(morgan('dev'));
+}
 var fs = require('fs')
 // create a write stream (in append mode)
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
